8cde5d8b47a01ea7dabf843b5c1ac7d3a92ffd3d
Sohag-84 <ihsohag84@gmail.com>
2024-02-10 22:44:38 +0600

    check user already login or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_web_clone/default_color/default_colors.dart';
@@ -5,7 +6,7 @@ import 'package:whatsapp_web_clone/routes/wep_routes.dart';
 
 String firstRoute = "/";
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: const FirebaseOptions(
@@ -16,6 +17,10 @@ void main() async {
     messagingSenderId: "581397415635",
     appId: "1:581397415635:web:a4d2a5df38f0cfcb8d3626",
   ));
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser != null) {
+    firstRoute = "/home";
+  }
   runApp(const MyApp());
 }
 

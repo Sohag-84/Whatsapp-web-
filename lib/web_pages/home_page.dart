c082a24cc11b170909ca5fe1b7deb2c0ea176043
Sohag-84 <ihsohag84@gmail.com>
2024-02-11 23:08:17 +0600

    set home page background color

--- lib/web_pages/home_page.dart
+++ lib/web_pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_web_clone/default_color/default_colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: DefaultColors.lightBarBackgroundColor,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                color: DefaultColors.primaryColor,
+                height: MediaQuery.sizeOf(context).height * 0.2,
+                width: MediaQuery.sizeOf(context).width,
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.sizeOf(context).height * 0.05,
+              bottom: MediaQuery.sizeOf(context).height * 0.05,
+              left: MediaQuery.sizeOf(context).height * 0.05,
+              right: MediaQuery.sizeOf(context).height * 0.05,
+              child: const Row(
+                children: [],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
